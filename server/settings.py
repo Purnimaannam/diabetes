9471fd3427c92b1ed4368502cf84a1fb1d3ac682
Purnima Annam <purnimaannam20@gmail.com>
2025-07-05 20:34:59 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c+h7(_fyvb2u2%-1n%d@6hbbmo^5+ou!1_=9b!a(p9rw3bav(*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
